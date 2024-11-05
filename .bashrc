e3dc905f7c0ab47692e25ca3788550757cc65e76
Andrew Virts <asvirts@outlook.com>
2024-11-04 18:59:22 -0600

    reset

--- .bashrc
+++
@@ -1,3 +0,0 @@
-cexport GEM_HOME="$(ruby -e 'puts Gem.user_dir')"
-export PATH="$PATH:$GEM_HOME/bin"
-
--- test/controllers/funding_application_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class FundingApplicationControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
